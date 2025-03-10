7e680116b2cb7ba2bc6ac5c658ebc676a8fc9b8f
Joewid Sharza <joewidsharza@gmail.com>
2025-03-10 13:51:50 -0400

    Planets Added

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import cors from "cors";
 dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
 const PORT = 3000;
@@ -16,60 +17,69 @@ app.listen(PORT, () => {
 });
 app.use(express.json());
 
+
 app.get("/api/planets", async (req, res) => {
   try {
-    res.status(200).send({
-      status: "success",
-      message: "planet receieved",
-    });
-  } catch (e) {
-    console.log(e);
-  }
-});
-
-app.get("/api/characters", async (req, res) => {
-  try {
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
-    const collection = db.collection("characters");
-    const charactersArray = await collection.find({}).toArray();
-    console.log(charactersArray);
-    res.json(charactersArray);
+    const collection = db.collection("planets");
+    const planetsArray = await collection.find({}).toArray();
+    console.log(planetsArray);
+    res.json(planetsArray);
   } catch (e) {
     console.log(e);
   }
 });
 
-app.get("/api/characters/:id", async (req, res) => {
+app.get("/api/planets/:id", async (req, res) => {
   try {
     const { id } = req.params;
     console.log(id);
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
-    const collection = db.collection("characters");
-    const character = await collection.findOne({ id: parseInt(id) });
-    console.log(character);
-    res.json(character);
+    const collection = db.collection("planets");
+    const planet = await collection.findOne({ id: parseInt(id) });
+    console.log(planet);
+    res.json(planet);
   } catch (e) {
     console.log(e);
   }
 });
 
-app.get("/api/characters/:id/films", async (req, res) => {
+app.get("/api/planets/:id/films", async (req, res) => {
   try {
     const { id } = req.params;
     console.log(id);
     const client = await MongoClient.connect(url);
     const db = client.db(dbName);
-    const collection = db.collection("films_characters");
-    const characterFilmsList = await collection
-      .find({ character_id: parseInt(id) })
+    const collection = db.collection("films_planets");
+    const planetfilmsList = await collection
+      .find({ planet_id: parseInt(id) })
       .toArray();
-    const filmIds = characterFilmsList.flatMap(
-      (character) => character.film_id
+    const filmsIds = planetfilmsList.flatMap(
+      (planet) => planet.film_id
     );
-    console.log(filmIds);
-    res.json(filmIds);
+    const filmsCollection = db.collection("films");
+    const films = await filmsCollection
+      .find({ id: { $in: filmsIds } })
+      .toArray();
+    console.log(films);
+    res.json(films);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/api/planets/:id/characters", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection("characters");
+    const characters = await collection.find({ homeworld:  parseInt(id)  }).toArray();
+    console.log(characters)
+    res.json(characters);
   } catch (e) {
     console.log(e);
   }
