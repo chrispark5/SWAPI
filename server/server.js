c4cedacd08e184546bc09b3721655031aa9f26cb
bbuy11 <benbaiel11@gmail.com>
2025-03-10 11:37:25 -0400

    Added path for characters

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { promises as fs } from "fs";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
+
 dotenv.config();
 const url = process.env.MONGO_DB_URL;
 const dbName = process.env.MONGO_DB;
-const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
 const PORT = 3000;
@@ -20,6 +22,19 @@ app.get("/api/planets", async (req, res) => {
       status: "success",
       message: "planet receieved",
     });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/api/characters", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const collection = db.collection("characters");
+    const charactersArray = await collection.find({}).toArray();
+    console.log(charactersArray);
+    res.json(charactersArray);
   } catch (e) {
     console.log(e);
   }
