6a4c159b91554e8c005e7249bd13e88c77d3ff54
Chris Park <5chrispark5@gmail.com>
2025-03-10 14:21:25 -0400

    all endpoints

---
+++ react/src/components/Characters.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export default function Characters() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/characters`);
+        if (!response.ok) {
+          throw new Error("Data could not be fetched!");
+        }
+        const json_response = await response.json();
+        setData(json_response);
+      } catch (error) {
+        console.error("Error fetching socks:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {data.map((character) => (
+        <p>{character.name}</p>
+      ))}
+    </>
+  );
+}
