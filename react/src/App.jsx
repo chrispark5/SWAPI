82c2673bffdb0f1eeca3315530ffc721f568a3f9
bbuy11 <benbaiel11@gmail.com>
2025-03-11 09:07:32 -0400

    Cleaned up Film

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,29 +7,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Film from "./components/Film";
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/api/films/1`);
-        if (!response.ok) {
-          throw new Error("Data could not be fetched!");
-        }
-        const json_response = await response.json();
-        setData(json_response);
-      } catch (error) {
-        console.error("Error fetching socks:", error);
-      }
-    };
-    fetchData();
-  }, []);
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/film" element={<Film data={data} />} />
+          <Route path="/film/:id" element={<Film />}></Route>
         </Routes>
       </Router>
     </>
