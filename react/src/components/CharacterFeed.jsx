73472be6c6dec99f621d34c3b7798638d52bab61
bbuy11 <benbaiel11@gmail.com>
2025-03-11 10:09:44 -0400

    Fixed pathing for CharacterFeed and FilmFeed

--- react/src/components/CharacterFeed.jsx
+++ react/src/components/CharacterFeed.jsx
@@ -1,19 +1,48 @@
 import { useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard";
+import { useParams } from "react-router-dom";
 
-export default function CharacterFeed() {
+export default function CharacterFeed(props) {
   const [data, setData] = useState([]);
+  const [charactersWithFilm, setCharactersWithFilm] = useState([]);
+  const [charactersWithPlanet, setCharactersWithPlanet] = useState([]);
+  const { id } = useParams();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/api/characters`);
+        const response = await fetch(`http://localhost:3000/api/character/`);
         if (!response.ok) {
           throw new Error("Data could not be fetched!");
         }
         const json_response = await response.json();
         setData(json_response);
       } catch (error) {
-        console.error("Error fetching socks:", error);
+        console.error("Error fetching characters:", error);
+      }
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/films/${props.filmId}/characters`
+        );
+        if (!response.ok) {
+          throw new Error("Data could not be fetched!");
+        }
+        const json_response = await response.json();
+        setCharactersWithFilm(json_response);
+      } catch (error) {
+        console.error("Error fetching characters by film:", error);
+      }
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/planets/${props.planetId}/characters`
+        );
+        if (!response.ok) {
+          throw new Error("Data could not be fetched!");
+        }
+        const json_response = await response.json();
+        setCharactersWithPlanet(json_response);
+      } catch (error) {
+        console.error("Error fetching planets:", error);
       }
     };
     fetchData();
@@ -21,9 +50,16 @@ export default function CharacterFeed() {
 
   return (
     <>
-      {data.map((character) => (
-        <CharacterCard key={character.id} character={character} />
-      ))}
+
+      {charactersWithFilm &&
+        charactersWithFilm.map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
+
+      {charactersWithPlanet &&
+        charactersWithPlanet.map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
     </>
   );
 }
